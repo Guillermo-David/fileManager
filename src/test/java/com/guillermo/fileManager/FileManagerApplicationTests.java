e15c3aeecbeb956957e9e554665164dc7ff54519
Guillermo PC <davidgarciaguillermo@gmail.com>
2023-02-17 14:39:25 +0100

    funcionando

--- src/test/java/com/guillermo/fileManager/FileManagerApplicationTests.java
+++ src/test/java/com/guillermo/fileManager/FileManagerApplicationTests.java
@@ -1,10 +1,5 @@
 package com.guillermo.fileManager;
 
-<<<<<<< HEAD:src/test/java/com/fileManager/FileManagerApplicationTests.java
-=======
-import org.junit.jupiter.api.Test;
-
->>>>>>> prueba:src/test/java/com/guillermo/fileManager/FileManagerApplicationTests.java
 //@SpringBootTest
 class FileManagerApplicationTests {
 
